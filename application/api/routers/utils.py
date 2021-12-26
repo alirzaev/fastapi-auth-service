385672874cb17305143ee231ac6a370450de663c
AliRzaev <alirzaev1996@gmail.com>
2021-12-26 17:15:17 +0300

    turn API endpoints' names into camelCase format

--- application/api/routers/utils.py
+++ application/api/routers/utils.py
@@ -10,7 +10,7 @@ from application.utils import send_test_email
 router = APIRouter(tags=['utils'])
 
 
-@router.post('/test-email', status_code=204)
+@router.post('/testEmail', status_code=204)
 def test_email(
         email_to: EmailStr,
         background_tasks: BackgroundTasks,
@@ -20,7 +20,7 @@ def test_email(
     return Response(status_code=204)
 
 
-@router.post("/test-celery/", status_code=204)
+@router.post("/testCelery/", status_code=204)
 def test_celery(
         message: str = Body(...),
         _: User = Depends(get_current_user)
